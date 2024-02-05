5525e7742ccd5023f28e6259769884287f60eb1a
I212470 <i212470@nu.edu.pk>
2024-02-06 00:08:25 +0500

    Kainat Ali, 21I-2470, Section B SMD, Assignment 1

--- app/src/main/java/com/KainatAli/i212470/MainActivity.kt
+++ app/src/main/java/com/KainatAli/i212470/MainActivity.kt
@@ -2,10 +2,35 @@ package com.KainatAli.i212470
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private val introPage_displayTime: Long = 5000  // 2 seconds
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            //Create an Intenet to navigate to Login Page
+            val intent = Intent(this@MainActivity, Screen2_Login::class.java)
+            startActivity(intent)
+
+            // Finish the current activity to prevent going back to the splash screen
+            finish()
+        }, introPage_displayTime)
+
+//        var btn = findViewById<CircleImageView>(R.id.main)
+//
+//        btn.setOnClickListener {
+//            startActivity(
+//
+//                Intent(this,
+//                    SecondActivity::class.java)
+//            );
+//        }
+
     }
 }
