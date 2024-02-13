df65c0b8ed86a9bf11c7b5a66c709ad8b9f58472
I212470 <i212470@nu.edu.pk>
2024-02-13 13:16:48 +0500

    Kainat Ali, 21I-2470, Section B SMD, Assignment 1

--- app/src/main/java/com/KainatAli/i212470/Screen7_Homepage.kt
+++ app/src/main/java/com/KainatAli/i212470/Screen7_Homepage.kt
@@ -1,7 +1,9 @@
 package com.KainatAli.i212470
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class Screen7_Homepage : AppCompatActivity() {
@@ -12,10 +14,47 @@ class Screen7_Homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen7_homepage)
 
+        // to display the name of user
+        // retrieve the name of user associated with the email entered
         displayName = findViewById<TextView>(R.id.displayName)
         val intent = intent
         val name = intent.getStringExtra(Screen2_Login.PASS_NAME)
         displayName.text = name.toString()
+
+        // Redirecting to Search Page for searching
+        val redirectToSearching = findViewById<Button>(R.id.Searchbtn_onHomePage)
+        redirectToSearching.setOnClickListener{
+            startActivity(Intent(this, Screen8_Searching::class.java));
+            finish()
+        }
+
+        // Redirecting to Chat Page
+//        val redirectToChatPage = findViewById<Button>(R.id.Chatbtn_onHomePage)
+//        redirectToChatPage.setOnClickListener{
+//            startActivity(Intent(this, Screen8_Searching::class.java));
+//            finish()
+//        }
+
+        // Redirecting to Profile Page
+//        val redirectToProfilePage = findViewById<Button>(R.id.Profilebtn_onHomePage)
+//        redirectToProfilePage.setOnClickListener{
+//            startActivity(Intent(this, Screen8_Searching::class.java));
+//            finish()
+//        }
+
+        // Redirect to Add mentor page, the plus button
+//        val redirectToAddMentor = findViewById<Button>(R.id.Plusbtn_onHomePage)
+//        redirectToAddMentor.setOnClickListener{
+//            startActivity(Intent(this, Screen8_Searching::class.java));
+//            finish()
+//        }
+
+        // Redirecting to Notifications Page
+//        val redirectToNotifications = findViewById<Button>(R.id.notificationbtn_onHomePage)
+//        redirectToNotifications.setOnClickListener{
+//            startActivity(Intent(this, Screen8_Searching::class.java));
+//            finish()
+//        }
 
     }
 }
